5b4d81c3584f715335b2eea8b033d7a5e156c8ac
Ash <33597520+Ashterism@users.noreply.github.com>
2025-06-17 09:45:03 +0400

    updating wifi check code

--- run_all.sh
+++ run_all.sh
@@ -3,13 +3,14 @@
 # --- Check Wi-Fi ---
 ping -c 1 -W 5 8.8.8.8 > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-  echo "Wi-Fi down. Restarting dhcpcd..."
-  sudo systemctl restart dhcpcd
-  sleep 10  # Give it a moment to come back
+  echo "[WARN] Wi-Fi down. Reinitialising wlan0 interface..."
+  sudo ip link set wlan0 down
+  sleep 2
+  sudo ip link set wlan0 up
+  sleep 10  # Give time to reconnect
 else
-  echo "Wi-Fi OK."
+  echo "[INFO] Wi-Fi OK."
 fi
-
 
 cd /home/ash/timelapse
 
