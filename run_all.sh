e2221efb81cee86e60af023879e068a6bf1b725d
Ash <33597520+Ashterism@users.noreply.github.com>
2025-06-17 09:27:38 +0400

    updating run_all to include a wifi check as seems to drop despite pi auto-connect functionality

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# --- Check Wi-Fi ---
+ping -c 1 -W 5 8.8.8.8 > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Wi-Fi down. Restarting dhcpcd..."
+  sudo systemctl restart dhcpcd
+  sleep 10  # Give it a moment to come back
+else
+  echo "Wi-Fi OK."
+fi
+
+
 cd /home/ash/timelapse
 
 # Update from GitHub
