6dc8e4dca89e8646dca206720a9697d1eb1cda45
Ash <33597520+Ashterism@users.noreply.github.com>
2025-06-24 10:17:55 +0400

    more broken paths

--- interfaces/web/webserver.py
+++ interfaces/web/webserver.py
@@ -4,7 +4,7 @@ from dotenv import set_key, load_dotenv
 import os
 import logging
 
-load_dotenv('/home/ash/timelapse/operations/config.env')
+load_dotenv('/home/ash/timelapse/config.env')
 app = Flask(__name__)
 
 # Log to file
@@ -45,7 +45,7 @@ def latest_timestamp():
 def photo():
     app.logger.debug("📸 /photo route hit")
     result = subprocess.run(
-        ['/bin/bash', '/home/ash/timelapse/functions/photo.sh'],
+        ['/bin/bash', '/home/ash/timelapse/timelapse/functions/photo.sh'],
         capture_output=True,
         text=True
     )
@@ -61,7 +61,7 @@ def photo():
 @app.route('/webserverlog')
 def show_log():
     try:
-        with open('/home/ash/timelapse/logs/webserver.log', 'r') as f:
+        with open('/home/ash/timelapse/data/logs/webserver.log', 'r') as f:
             lines = f.readlines()[-50:]  # Show last 50 lines
         return '<pre>' + ''.join(lines) + '</pre>'
     except FileNotFoundError:
@@ -69,7 +69,7 @@ def show_log():
 
 @app.route('/mode/wifi')
 def switch_to_wifi():
-    set_key('/home/ash/timelapse/operations/config.env', 'wifi_mode', 'client')
+    set_key('/home/ash/timelapse/config.env', 'wifi_mode', 'client')
     return 'Switched to Wi-Fi. Reboot or wait for next tma1.sh.'
 
 @app.route('/status')
