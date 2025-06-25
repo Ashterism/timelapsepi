2901ca1c8d6f709349132b01951b685deadcaf33
Ash Bladon <ashleybladon@gmail.com>
2025-06-25 16:23:03 +0400

    yet more logging

--- sandbox/sys_monitor.sh
+++ sandbox/sys_monitor.sh
@@ -12,5 +12,7 @@ while true; do
   curl -s -o /dev/null -w "%{http_code} (time: %{time_total}s)\n" http://127.0.0.1:5000/health >> "$LOGFILE"
   df -h / >> "$LOGFILE"
   vcgencmd measure_temp >> "$LOGFILE" 2>/dev/null
+  echo "=== DMESG SNAPSHOT ===" >> "$LOGFILE"
+  dmesg | tail -n 20 >> "$LOGFILE"
   sleep 60
 done
