67ab5373208838d5c65ff6de6d2ef48a86c31408
Ash Bladon <ashleybladon@gmail.com>
2025-07-03 09:44:47 +0400

    flash led

--- interfaces/buttons/toggleHotWifi.py
+++ interfaces/buttons/toggleHotWifi.py
@@ -2,6 +2,9 @@
 
 import subprocess
 
+with open("/home/ash/button_trigger.log", "a") as f:
+    f.write("Button script was called\n")
+
 def flash_led_script():
     subprocess.run(
         ["/usr/bin/python3", "/home/ash/timelapse/interfaces/buttons/flash_led.py"]
