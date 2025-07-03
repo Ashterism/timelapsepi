e8c11a18b99db5ad776b7087d224c0a052919500
Ash Bladon <ashleybladon@gmail.com>
2025-07-03 09:38:34 +0400

    changed led to d2

--- interfaces/buttons/flash_led.py
+++ interfaces/buttons/flash_led.py
@@ -7,9 +7,9 @@ pj = PiJuice(1, 0x14)  # I2C bus 1, default PiJuice address
 
 def flash_led(times=3, on_time=0.3, off_time=0.3):
     for _ in range(times):
-        pj.config.SetLedState('D1', 'ON')
+        pj.config.SetLedState('D2', 'ON')
         time.sleep(on_time)
-        pj.config.SetLedState('D1', 'OFF')
+        pj.config.SetLedState('D2', 'OFF')
         time.sleep(off_time)
 
 if __name__ == "__main__":
