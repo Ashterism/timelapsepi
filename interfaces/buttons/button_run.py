d0660dbc8594c12f6ca266dcf8ad9c53fa80db65
Ash Bladon <ashleybladon@gmail.com>
2025-07-04 11:26:56 +0400

    added ssh watchdog (reconnect) to the button

--- interfaces/buttons/button_run.py
+++ interfaces/buttons/button_run.py
@@ -5,7 +5,11 @@
 import subprocess
 
 def call_script(script_path):
-    subprocess.run(["/usr/bin/python3", script_path])
+    if script_path.endswith(".py"):
+        subprocess.run(["/usr/bin/python3", script_path])
+    elif script_path.endswith(".sh"):
+        subprocess.run(["/bin/bash", script_path])
 
 if __name__ == "__main__":
     call_script("/home/ash/timelapse/interfaces/buttons/flash_led.py")
+    call_script("/home/ash/timelapse/operations/ssh_watchdog.sh")
